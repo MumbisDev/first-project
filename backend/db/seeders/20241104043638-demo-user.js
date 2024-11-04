ab374526e8284e7df89eab3ffb4d087ff643ce55
liawill114 <lia.will114@gmail.com>
2024-11-04 16:02:16 -0700

    added fields to seed data

--- backend/db/seeders/20241104043638-demo-user.js
+++ backend/db/seeders/20241104043638-demo-user.js
@@ -13,16 +13,22 @@ module.exports = {
     await User.bulkCreate(
       [
         {
+          firstName: "Bob",
+          lastName: "Bobert",
           email: "demo@user.io",
           username: "Demo-lition",
           hashedPassword: bcrypt.hashSync("password"),
         },
         {
+          firstName: "Jane",
+          lastName: "Doe",
           email: "user1@user.io",
           username: "FakeUser1",
           hashedPassword: bcrypt.hashSync("password2"),
         },
         {
+          firstName: "Laura",
+          lastName: "Michaels",
           email: "user2@user.io",
           username: "FakeUser2",
           hashedPassword: bcrypt.hashSync("password3"),
